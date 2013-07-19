dc9ad111b8aec9590dad7ea9efcc7c39b810ca61
Wael Amine Boutglay <btwael@gmail.com>
2013-07-19 00:17:18 +0000

    FIX: error throwing

--- build.js
+++ build.js
@@ -20,31 +20,32 @@ try {
 	fs.unlinkSync('extras/mammouth-nodejs.js');
 } catch(err) {
 	console.log(err);
-}
-try {
-	var data = '';
-	data += 'var mammouth = {};' + "\n";
-	rf = fs.readFileSync('src/parser.pegjs', 'utf8')
-	r = PEG.buildParser(rf, {})._source;
-	fs.writeFile('src/parser.js', 'mammouth.parser' + " = " + r + ';', function () {
-	});
-	data += 'mammouth.parser' + " = " + r + ';';
-	rf = fs.readFileSync('src/LineTerminator.pegjs', 'utf8')
-	r = PEG.buildParser(rf, {})._source;
-	fs.writeFile('src/LineTerminator.js', 'mammouth.LineTerminatorParser' + " = " + r + ';', function () {
-	});
-	data += 'mammouth.LineTerminatorParser' + " = " + r + ';';
-	data += fs.readFileSync('src/tokens.js').toString() + '\n';
-	data += fs.readFileSync('src/compiler.js').toString() + '\n';
-	fs.appendFileSync("extras/mammouth.js", data + "\n");
-} catch(err) {
-	console.log(err);
-}
-try {
-	var data = '';
-	data += fs.readFileSync('extras/mammouth.js').toString() + '\n';
-	data += 'module.exports = mammouth;' + "\n"; 
-	fs.appendFileSync("extras/mammouth-nodejs.js", data);
-} catch(err) {
-	console.log(err);
+} finally {
+	try {
+		var data = '';
+		data += 'var mammouth = {};' + "\n";
+		rf = fs.readFileSync('src/parser.pegjs', 'utf8')
+		r = PEG.buildParser(rf, {})._source;
+		fs.writeFile('src/parser.js', 'mammouth.parser' + " = " + r + ';', function () {
+		});
+		data += 'mammouth.parser' + " = " + r + ';';
+		rf = fs.readFileSync('src/LineTerminator.pegjs', 'utf8')
+		r = PEG.buildParser(rf, {})._source;
+		fs.writeFile('src/LineTerminator.js', 'mammouth.LineTerminatorParser' + " = " + r + ';', function () {
+		});
+		data += 'mammouth.LineTerminatorParser' + " = " + r + ';';
+		data += fs.readFileSync('src/tokens.js').toString() + '\n';
+		data += fs.readFileSync('src/compiler.js').toString() + '\n';
+		fs.appendFileSync("extras/mammouth.js", data + "\n");
+	} catch(err) {
+		console.log(err);
+	}
+	try {
+		var data = '';
+		data += fs.readFileSync('extras/mammouth.js').toString() + '\n';
+		data += 'module.exports = mammouth;' + "\n"; 
+		fs.appendFileSync("extras/mammouth-nodejs.js", data);
+	} catch(err) {
+		console.log(err);
+	}
 }
