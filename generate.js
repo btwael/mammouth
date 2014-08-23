46ee92e7eecf2b5ce494172162c689ba85354a3b
Wael Boutglay <btwael@gmail.com>
2014-08-23 12:25:14 +0200

    set version to 2.0.0

--- generate.js
+++ generate.js
@@ -2,6 +2,7 @@ yy = require('./lib/nodes');
 parser = require('./lib/grammar');
 parser.lexer = require('./lib/lexer');
 parser.yy = yy;
+// Generate the stand-alone parser
 require('fs').writeFile('./lib/parser.js', parser.generate(), function(err) {
     if(err) {
         console.log(err);
