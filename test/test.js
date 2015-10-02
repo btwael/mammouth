5468ed675ae09fd07408be05171035ac0b815984
Wael Boutglay <btwael@gmail.com>
2015-10-02 21:27:42 +0000

    Fix log date

--- test/test.js
+++ test/test.js
@@ -6,38 +6,12 @@ function heredoc (f) {
 code = heredoc(function() {
 /*
 {{
-func mammouth() ->
-    arguments = func_get_args()
-    switch arguments[0]
-        when '+'
-            if (is_string(arguments[1]) and is_numeric(arguments[2])) or (is_string(arguments[1]) and is_numeric(arguments[1]))
-                arguments[1] ~~ arguments[2]
-            else
-                'strict mode'
-                arguments[1] + arguments[2]
-        when 'length'
-            if is_array(arguments[1])
-                count(arguments[1])
-            else if is_string(arguments[1])
-                strlen(arguments[1])
-            else if is_numeric(arguments[1])
-                strlen(arguments[1] => string)
-        when 'slice'
-            if is_array(arguments[1])
-                if count(arguments) is 3
-                    array_slice(arguments[1], arguments[2])
-                else
-                    array_slice(arguments[1], arguments[2], arguments[3] - arguments[2])
-            else if is_string(arguments[1])
-                if count(arguments) is 3
-                    substr(arguments[1], arguments[2])
-                else
-                    substr(arguments[1], arguments[2], arguments[3] - arguments[2])
-            else if is_numeric(arguments[1])
-                if count(arguments) is 3
-                    mammouth('slice', arguments[1] => string, arguments[2])
-                else
-                     mammouth('slice', arguments[1] => string, arguments[2], arguments[3] - arguments[2])
+str = 'tex1' ~~ 'text2' # in all modes
+
+str = 'tex1' + 123
+
+'strict mode'
+str = 'tex1' + 123 
 }}
 */
 })
