57a08db7b51c214331d78d5f860a97fbae91ce94
Kostas Minaidis <kostas@creativecollective.gr>
2016-04-30 23:18:56 +0200

    Adding auto compiler ST3 plugin

---
+++ plugins/sublime-text-autocompiler/buildmammouthonsave.py
@@ -0,0 +1,17 @@
+# REQUIREMENTS: npm install -g mammouth
+# USAGE: Files must have a .mammouth suffix
+
+import sublime, sublime_plugin,os
+from os.path import dirname, realpath
+
+class BuildMammouthOnSave(sublime_plugin.EventListener):
+ 
+  def on_post_save(self, view):
+    mammouthFile = view.file_name()
+    filename, file_extension = os.path.splitext(mammouthFile)
+    if file_extension == ".mammouth":
+      print("Compiling: " + mammouthFile)
+      view.window().run_command('exec',{'cmd': ["/usr/local/bin/mammouth", "-c", mammouthFile] })
+
+# REFERENCES
+# http://www.purplebeanie.com/Development/automatically-run-build-on-save-in-sublime-text-2.html
